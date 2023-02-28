2861d47a94ad0123897118ec43be6e6665156ed8
Shraddha Thyagarajan <shraddhathyagarajan@Shraddhas-MacBook-Pro.local>
2023-02-28 02:10:20 -0500

    Changes to index.html

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -14,7 +14,6 @@ function Footer() {
     <Container fluid className="footer">
       <Row>
         <Col md="4" className="footer-copywright">
-          {/* <h3>Designed and Developed by Soumyajit Behera</h3> */}
         </Col>
         <Col md="4" className="footer-copywright">
           <h3>Copyright © {year} </h3>
